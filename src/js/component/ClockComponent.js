a3a04cb6d55742c2795e4597630e701b876096a6
Antoniya <anemtserova@gmail.com>
2021-05-07 16:28:07 +0000

    first commit

---
+++ src/js/component/ClockComponent.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+const ClockComponent = () => {
+	let [currentTime, setTimeout] = useState(new Date());
+
+	useEffect(() => {
+		setTimeout(() => new Date(), 1000);
+	}, [currentTime]);
+
+	// const textColor = {
+	// 	color: "red"
+	// };
+
+	return <div>{currentTime.toString()}</div>;
+};
+
+export default ClockComponent;
